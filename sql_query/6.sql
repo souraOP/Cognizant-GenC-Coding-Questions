989913ce0bf04aa172d31bc429bb43201b5830bf
souraOP <sourasishmondal95@gmail.com>
2024-10-27 03:20:20 +0530

    more sql codes

---
+++ sql_query/6.sql
@@ -0,0 +1,21 @@
+/*
+Write an SQL Query to display:
+
+The branch ID and the count of accounts handled by each branch ordered in ascending order of branch ID.
+
+Your output should have 2 columns as given below:
+
+BRANCH_ID  |  COUNT
+
+You can view the database schema by clicking the View Schema tab at the bottom of the query window on the right-hand side of the screen.
+
+*/
+
+SELECT
+    branch_id, COUNT(*) AS COUNT
+FROM
+    accounts
+GROUP BY
+    branch_id
+ORDER BY
+    branch_id ASC;
---
+++ sql_query/7.sql
@@ -0,0 +1,27 @@
+/*
+Write an SQL Query to display:
+
+The number of passengers in each flight. Display the result in ascending order of flight id.
+
+Note: Do not display flights with no passengers.
+
+Your output should have two columns as given below:
+
+FLIGHT_ID  |  NUMBER_OF_PASSENGERS
+
+You can view the database schema by clicking the View Schema tab at the bottom of the query window on the right-hand side of the screen.
+
+*/
+
+SELECT
+    f.FLIGHT_ID, COUNT(p.PASSENGER_ID) AS NUMBER_OF_PASSENGERS
+FROM
+    flights f
+JOIN
+    passengers p ON f.FLIGHT_ID = p.FLIGHT_ID
+GROUP BY
+    f.FLIGHT_ID
+HAVING
+    COUNT(p.PASSENGER_ID) > 0
+ORDER BY
+    f.FLIGHT_ID ASC;
